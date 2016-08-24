7462bd287fec29e6fb7bbef93554356a532a41b1
Onurhan Aytac <onurhan.aytac@agri-t.com>
2016-08-24 12:03:07 +0300

    oopps

--- views/gitCodes/build/temp/development/SSW/sass/config.rb
+++
@@ -1,2 +0,0 @@
-Compass.add_project_configuration('../../../../../sass/config.rb')
-cache_path = '/home/localadmin/gitCodes/SSW/views/gitCodes/build/.sass-cache'
--- views/gitCodes/build/temp/development/SSW/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /home/localadmin/gitCodes/SSW/views/gitCodes/app.js
